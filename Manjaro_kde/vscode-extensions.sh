4645baf0fda195d5363e800552b657c9b9f4a6b1
Kevin Martinez <kkevinmartinez7@gmail.com>
2022-06-18 18:59:28 -0600

    vscode extensions

--- Manjaro_kde/vscode-extensions.sh
+++ Manjaro_kde/vscode-extensions.sh
@@ -1,4 +1,3 @@
-#---extensions---
 code --install-extension thekalinga.bootstrap4-vscode
 code --install-extension lihui.vs-color-picker
 code --install-extension vincaslt.highlight-matching-tag
@@ -10,26 +9,37 @@ code --install-extension HookyQR.beautify
 code --install-extension Prisma.vscode-graphql
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension esbenp.prettier-vscode
-code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension christian-kohler.path-intellisense
 code --install-extension SetiawanJodi.myci-extension
 code --install-extension small.php-ci
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension alefragnani.bookmarks
+code --install-extension ritwickdey.liveserver
+code --install-extension whosydd.stylelint-config
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension github.vscode-pull-request-github
+code --install-extension formulahendry.code-runner
 #Normal
 code --install-extension Tyriar.lorem-ipsum
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension wakatime.vscode-wakatime
 code --install-extension alefragnani.project-manager
+code --install-extension bierner.emojisense
 #Syntaxis
-code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension naumovs.color-highlight
 code --install-extension mikestead.dotenv
 code --install-extension editorconfig.editorconfig
 #Theme
 code --install-extension equinusocio.vsc-material-theme
 code --install-extension whizkydee.material-palenight-theme
+#GIT
+code --install-extension eamodio.gitlens
 #HTML
 code --install-extension ecmel.vscode-html-css
 code --install-extension abusaidm.html-snippets
@@ -52,15 +62,18 @@ code --install-extension joshjg.generate-react-component
 code --install-extension ofhumanbondage.react-proptypes-intellisense
 code --install-extension msjsdiag.vscode-react-native
 code --install-extension steoates.autoimport
+code --install-extension arunagiritm.material-ui
 #Angular
 code --install-extension johnpapa.angular2
 code --install-extension mikael.angular-beastcode
 code --install-extension angular.ng-template
 #Vue
+code --install-extension jcbuisson.vue
 code --install-extension octref.vetur
 code --install-extension hollowtree.vue-snippets
 code --install-extension sdras.vue-vscode-snippets
 code --install-extension dariofuzinato.vue-peek
+code --install-extension vuetifyjs.vuetify-vscode
 #TypeScript
 code --install-extension kevinmcgowan.typescriptimport
 #Flutter
@@ -87,6 +100,8 @@ code --install-extension onecentlin.laravel-blade
 code --install-extension kokororin.vscode-phpfmt
 code --install-extension amiralizadeh9480.laravel-extra-intellisense
 code --install-extension onecentlin.laravel-extension-pack
+code --install-extension rexshi.phpdoc-comment-vscode-plugin
+code --install-extension devsense.phptools-vscode
 #Firebase
 code --install-extension toba.vsfire
 #Icons
@@ -107,3 +122,9 @@ code --install-extension bradlc.vscode-tailwindcss
 #Docker
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+#XML
+code --install-extension dotjoshjohnson.xml
+code --install-extension redhat.vscode-xml
+#NodeJS
+code --install-extension module-intellisense
+code --install-extension abdoseadaa.node-js-snippet
