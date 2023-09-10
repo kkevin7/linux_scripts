6859f9c8c0b6a1c70dfc24d3c4aa129ae3edddcf
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-09-10 10:20:56 -0600

    gnome extensions urls

--- debian/gnome-extensions.sh
+++ debian/gnome-extensions.sh
@@ -1,4 +1,16 @@
-cd /tmp
-git clone https://github.com/micheleg/dash-to-dock.git
-make
-make install
+#--------------------- Dash to dock ------------------
+https://extensions.gnome.org/extension/307/dash-to-dock/
+#cd /tmp
+#git clone https://github.com/micheleg/dash-to-dock.git
+#make
+#make install
+
+#--------------------- Blur my Shell ------------------
+https://extensions.gnome.org/extension/3193/blur-my-shell/
+#cd /tmp
+#git clone https://github.com/aunetx/blur-my-shell
+#cd blur-my-shell
+#make install
+
+#--------------------- AppIndicator/KStatusNotifierItem support for GNOME Shell ------------------
+https://extensions.gnome.org/extension/615/appindicator-support/
