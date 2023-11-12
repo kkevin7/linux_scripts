86db482afd8c8be7991b392cc03434a40a01bed4
Kevin Martinez <kkevinmartinez7@gmail.com>
2023-11-12 10:10:19 -0600

    grub configuration

--- debian/debian-script.sh
+++ debian/debian-script.sh
@@ -1,5 +1,9 @@
 #-------------------- Debian Repositories  --------------------
-cp -fr ./repositories/debian12/sources.list /etc/apt/sources.list
+sudo cp -fr ./repositories/debian12/sources.list /etc/apt/sources.list
+
+#-------------------- Debian Grub configuration  --------------------
+sudo cp -r /etc/default/grub /etc/default/grub.bak
+sudo cp -fr ./grub /etc/default/grub
 
 #-------------------- Update OS  --------------------
 sudo apt-get update
@@ -243,4 +247,4 @@ sudo rm -r /etc/apt/sources.list.d/papirus-ppa.list
 sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu jammy main' > /etc/apt/sources.list.d/papirus-ppa.list"
 sudo wget -qO /etc/apt/trusted.gpg.d/papirus-ppa.asc 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9461999446FAF0DF770BFC9AE58A9D36647CAE7F'
 sudo apt-get update
-sudo apt-get install papirus-icon-theme
+sudo apt-get install -y papirus-icon-theme
